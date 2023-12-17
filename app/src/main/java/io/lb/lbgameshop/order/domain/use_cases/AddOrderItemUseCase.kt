0a2fb6d79c40e140194f299cf765d8190202f687
Leonardo Bai <69223422+LeonardoBai12@users.noreply.github.com>
2023-12-16 22:55:41 -0300

    Update new order use case

--- app/src/main/java/io/lb/lbgameshop/order/domain/use_cases/AddOrderItemUseCase.kt
+++ app/src/main/java/io/lb/lbgameshop/order/domain/use_cases/AddOrderItemUseCase.kt
@@ -1,13 +1,24 @@
 package io.lb.lbgameshop.order.domain.use_cases
 
+import io.lb.lbgameshop.game.domain.model.Game
 import io.lb.lbgameshop.order.domain.model.Order
+import io.lb.lbgameshop.order.domain.model.OrderItem
 import io.lb.lbgameshop.order.domain.repository.OrderRepository
 import io.lb.lbgameshop.sign_in.domain.model.UserData
 
 class AddOrderItemUseCase(
     val repository: OrderRepository
 ) {
-    suspend operator fun invoke(userData: UserData, order: Order) {
-        repository.addOrderItem(userData, order)
+    suspend operator fun invoke(
+        userData: UserData,
+        order: Order,
+        game: Game
+    ) {
+        val orderItem = OrderItem(
+            orderId = order.uuid,
+            data = game
+        )
+
+        repository.addOrderItem(userData, orderItem)
     }
 }
--- app/src/main/java/io/lb/lbgameshop/order/presentation/new_order/NewOrderState.kt
+++ app/src/main/java/io/lb/lbgameshop/order/presentation/new_order/NewOrderState.kt
@@ -1,4 +1,10 @@
 package io.lb.lbgameshop.order.presentation.new_order
 
-class NewOrderState {
-}
+import io.lb.lbgameshop.order.domain.model.Order
+import io.lb.lbgameshop.order.domain.model.OrderItem
+
+data class NewOrderState(
+    val order: Order? = Order(),
+    val items: List<OrderItem> = emptyList(),
+    val loading: Boolean = true
+)
